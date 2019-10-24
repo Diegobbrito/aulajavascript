7c4125e0ca07490637f477779942c8b7233f139d
Diego <dibbrito@gmail.com>
2019-10-23 21:58:34 -0300

    primeiro programa javascript

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+
+function clicou(){
+    alert("Obrigado por cliclar");
+}
+
+
+
+/*var d = new Date();
+alert(d.getMinutes());
+alert(d.getHours());
+*/
+/*
+var idade = prompt("Qual a sua idade");
+if (idade >=18){
+alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+/*
+var lista = ["maça","pera","laranja"];
+lista.push("uva");
+//lista.pop(); remove da pilha
+
+console.log(lista[0]);
+console.log(lista.toString()[0]);
+
+console.join(lista.join(" "));
+*/
+
+
+//var nome = "Diego Brito";
+//var n1 = 5
+//var n2 = 3;
+//var frase = "Brasil é o melhor time do mundo" 
+//alert(nome + " tem" + idade + "anor" );
+//console.log(n1*n2);
+//alert("Bem vindo "+ nome);
+//console.log(frase.toLoewrCase());
+
+//console.log(nome);
